48844ef5b87dc80c91979b89723eda59a3a36b0d
Ye Thi Ha Htwe <108426020+yethihahtwe@users.noreply.github.com>
2025-02-06 23:10:53 +0000

    Fri, Feb 7, 2025, 6:10 AM +07:00

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,5 @@
 
 import __ASTRO_IMAGE_IMPORT_Z22U8TS from "../assets/spectre.png?astroContentImageFlag=&importer=src%2Fcontent%2Fbooks%2Ftest.mdx";
-import __ASTRO_IMAGE_IMPORT_1y8Vzg from "../assets/spectre.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fspectre.mdx";
 import __ASTRO_IMAGE_IMPORT_ZYXk1i from "assets/the-wealth-of-knowledge/03-ui.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-wealth-of-knowledge.mdx";
-export default new Map([["../assets/spectre.png?astroContentImageFlag=&importer=src%2Fcontent%2Fbooks%2Ftest.mdx", __ASTRO_IMAGE_IMPORT_Z22U8TS], ["../assets/spectre.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fspectre.mdx", __ASTRO_IMAGE_IMPORT_1y8Vzg], ["assets/the-wealth-of-knowledge/03-ui.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-wealth-of-knowledge.mdx", __ASTRO_IMAGE_IMPORT_ZYXk1i]]);
+export default new Map([["../assets/spectre.png?astroContentImageFlag=&importer=src%2Fcontent%2Fbooks%2Ftest.mdx", __ASTRO_IMAGE_IMPORT_Z22U8TS], ["assets/the-wealth-of-knowledge/03-ui.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fthe-wealth-of-knowledge.mdx", __ASTRO_IMAGE_IMPORT_ZYXk1i]]);
 		
