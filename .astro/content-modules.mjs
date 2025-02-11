5e0dd0716e81dc668c8f7b8137258f0e4183c04d
Ye Thi Ha Htwe <108426020+yethihahtwe@users.noreply.github.com>
2025-02-11 15:26:16 +0000

    Tue, Feb 11, 2025, 10:26 PM +07:00

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -4,5 +4,6 @@ export default new Map([
 ["src/content/projects/spectre.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fspectre.mdx&astroContentModuleFlag=true")],
 ["src/content/other/about.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fother%2Fabout.mdx&astroContentModuleFlag=true")],
 ["src/content/posts/solving-leetcode-stone-game-iii.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsolving-leetcode-stone-game-iii.mdx&astroContentModuleFlag=true")],
-["src/content/posts/the-wealth-of-knowledge.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fthe-wealth-of-knowledge.mdx&astroContentModuleFlag=true")]]);
+["src/content/posts/the-wealth-of-knowledge.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fthe-wealth-of-knowledge.mdx&astroContentModuleFlag=true")],
+["src/content/posts/hmis-workshop-notes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fhmis-workshop-notes.mdx&astroContentModuleFlag=true")]]);
 		
